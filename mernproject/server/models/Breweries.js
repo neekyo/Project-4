4ff7387a97b7ffe34dbe0680dfc2d8d08c17aedd
Nick Sherman <ogleffeckt2@yahoo.com>
2019-10-10 16:51:56 -0400

    seeded 25k breweries

--- mernproject/server/models/Breweries.js
+++ mernproject/server/models/Breweries.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const breweriesSchema = new Schema ({
-  name: String, 
-  type: String,
+const breweriesSchema = new Schema({
+  address: String,
   city: String,
-  state: String,
-  picture: String, 
-  numberOfBeers: Number,
-  food: String,
-  signatureBeer: String,
-  website: String,
+  province: String, //actually just a state (usually)
+  name: String,
+  // website: String, might use website l8r
 });
 
 const Breweries = mongoose.model("Breweries", breweriesSchema);
