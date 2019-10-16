e0b3d3fb2c805f13b09ea6e0ed092f95ddf0fd06
Nick Sherman <ogleffeckt2@yahoo.com>
2019-10-16 19:40:30 -0400

    s

---
+++ client/build/precache-manifest.bcb030427d02ad51cdff81d645d32858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ad9c69f67fb76a12a158f77d5044b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c6cc1719d222382887e",
+    "url": "/static/css/main.029b2847.chunk.css"
+  },
+  {
+    "revision": "57774429e1da18d20e0c",
+    "url": "/static/js/2.740120b0.chunk.js"
+  },
+  {
+    "revision": "8c6cc1719d222382887e",
+    "url": "/static/js/main.706c1f8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
